ac7016f16f3d204b5b843a807c9e9bc7a843629a
Admin <natalieconan17@gmail.com>
2023-05-17 22:17:59 +0700

    update and clean code

--- script.js
+++ script.js
@@ -1,108 +1,165 @@
-const icon = [ './images/icon-moon.svg', './images/icon-sun.svg' ];
-const background_image = [ './images/bg-desktop-light.jpg', './images/bg-desktop-dark.jpg' ];
-const box_color = [ 'white', '#312d3e' ]
-const color = [ 'white', '#0f0e11' ];
-var id_icon = 0;
-var id_background_image = 0;
-var id_background_color = 0;
-var id_text_color = 1;
-var id_box_color = 0;
-var id_task = 7;
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const todoList = document.querySelector('.todo-list');
 
-function changeIconTheme() {
-    document.getElementById('theme-icon').src=icon[1-id_icon];
-    id_icon = 1-id_icon;
+Todo = localStorage.getItem('Todo');
+if (Todo == null || Todo == 'undefined') Todo = [];
+else Todo = JSON.parse(Todo);
+
+const images = [ 
+    "./images/icon-check.svg",
+    "./images/icon-moon.svg",
+    "./images/icon-sun.svg",
+    "./images/bg-desktop-light.jpg",
+    "./images/bg-desktop-dark.jpg",
+]
+
+const task = document.querySelector('.task');
+const cloneTask = task.cloneNode(true);
+task.parentNode.removeChild(task);
+
+function updateData() {
+    const name = document.querySelectorAll('.task-name');
+    Todo = [];
+
+    name.forEach((taskName) => {
+        let counter = 0;
+        if (taskName.classList.contains('active')) counter = 1;
+        Todo.push([taskName.textContent, counter]);
+    });
+
+    localStorage.setItem("Todo", JSON.stringify(Todo));
+
+    const count = document.querySelector('.item-left span');
+    count.textContent = Todo.length;
 }
- 
-function changeBackground() {
-    document.body.style.backgroundImage = `url(${background_image[1-id_background_image]})`;
-    id_background_image = 1-id_background_image;
 
-    document.body.style.backgroundColor = color[1-id_background_color];
-    id_background_color = 1-id_background_color;
+function circleClick(newTask) {
+    const img = newTask.querySelector('img');
+    const insider = newTask.querySelector('.insider');
+    const content = newTask.querySelector('.task-name');
+
+    img.classList.toggle('active');
+    insider.classList.toggle('active');
+    content.classList.toggle('active');
 }
 
-function changeBoxColor() {
-    document.getElementById('input').style.color = color[1-id_text_color];
-    var els = document.getElementsByTagName('li');
-    for (var i = 1; i < els.length; i++) {
-        els[i].style.color = color[1-id_text_color];
-        els[i].style.backgroundColor = box_color[1-id_box_color];
+function appendTask(taskName) {
+    const newTask = cloneTask.cloneNode(true);
+    newTask.querySelector('.task-name').textContent = taskName[0];
+    todoList.append(newTask);
+
+    const cross = newTask.querySelector('.cross-mark');
+    cross.addEventListener('click', () => {
+        todoList.removeChild(newTask);
+        updateData();
+    })
+
+    const circle = newTask.querySelector('.circle');
+    circle.addEventListener('click', () => {
+        circleClick(newTask);
+    })
+
+    if( taskName[1] ) circleClick( newTask );
+}
+
+form.addEventListener('submit', (val) => {
+    val.preventDefault();
+    
+    const taskName = input.value;
+    input.value = "";
+
+    if (taskName.length > 0) {
+        appendTask([taskName, 0]);
+        updateData();
     }
-    id_text_color = 1-id_text_color;
-    id_box_color = 1-id_box_color;
-}
+})
 
-function changeTheme() {
-    changeIconTheme();
-    changeBackground();
-    changeBoxColor();
-}
+const options = document.querySelectorAll('.status-options .option');
+options[0].classList.add('blue-text');
 
-function completedEvent(id) {
-    if (document.getElementById("task" + id).parentElement.style.textDecoration != 'line-through') {
-        document.getElementById("task" + id).parentElement.style.textDecoration = 'line-through';
-    } else {
-        document.getElementById("task" + id).parentElement.style.textDecoration = 'none';
+function clearColorText() {
+    for (var i = 0; i < 3; ++i) {
+        options[i].classList.remove('blue-text');
     }
 }
 
-function displayAllEvent() {
-    els = document.getElementsByTagName('li');
-    for (var i = 2; i < els.length - 1; i++) {
-        els[i].style.display = 'block';
-    }
-}
+options[0].addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
+    tasks.forEach((task) => {
+        task.style.display = 'block';
+    })
 
-function displayAllActive() {
-    els = document.getElementsByTagName('li');
-    for (var i = 2; i < els.length - 1; i++) {
-        if (els[i].style.textDecoration != 'line-through') {
-            els[i].style.display = 'block';
+    clearColorText();
+    options[0].classList.add('blue-text');
+})
+
+options[1].addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
+    tasks.forEach((task) => {
+        taskName = task.querySelector('.task-name');
+        if (taskName.classList.contains('active')) {
+            task.style.display = 'none';
         } else {
-            els[i].style.display = 'none';
+            task.style.display = 'block';
         }
-    }
-}
+    });
+    clearColorText();
+    options[1].classList.add('blue-text');
+})
 
-function displayAllCompleted() {
-    els = document.getElementsByTagName('li');
-    for (var i = 2; i < els.length - 1; i++) {
-        if (els[i].style.textDecoration == 'line-through') {
-            els[i].style.display = 'block';
+options[2].addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
+    tasks.forEach((task) => {
+        taskName = task.querySelector('.task-name');
+        if (taskName.classList.contains('active')) {
+            task.style.display = 'block';
         } else {
-            els[i].style.display = 'none';
+            task.style.display = 'none';
         }
-    }
-}
+    });
+    clearColorText();
+    options[2].classList.add('blue-text');
+})
 
-function removeAllCompleted() {
-    els = document.getElementsByTagName('li');
-    for (var i = 2; i < els.length - 1; i++) {
-        if (els[i].style.textDecoration == 'line-through') {
-            els[i].style.display = 'none';
+const clearCompleted = document.querySelector('.status-bar .clear');
+clearCompleted.addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task');
+    tasks.forEach((task) => {
+        if( task.querySelector(".task-name").classList.contains("active") ) {
+            todoList.removeChild( task );
         }
-    }
-}
+        else task.style.display = "block";
+    })
 
-function addEvent() {
-    var li = document.createElement('li');
-    li.style.backgroundColor = box_color[id_box_color];
-    li.style.color = color[id_text_color];
-    var input_value = document.getElementById('input').value;
-    var event = document.createTextNode(input_value);
-    var ip = document.createElement('input');
-    ++id_task;
-    ip.setAttribute('id', `task${id_task}`);
-    ip.setAttribute('type', 'checkbox');
-    ip.setAttribute('class', 'checkbox-round');
-    ip.setAttribute('onclick' ,`completedEvent(${id_task})`);
-    li.appendChild(ip);
-    li.appendChild(event);
-    if (input_value == '') {
-        alert('Write Something');
-    } else {
-        document.getElementById('task-list').appendChild(li);
+    updateData();
+    clearColorText();
+    options[0].classList.add('blue-text');
+})
+
+const modeIcon = document.querySelector(".text-container .mode img");
+var run = 0;
+
+modeIcon.addEventListener( "click" , () => {
+    run = 1 - run;
+    modeIcon.src = images[2 - run];
+
+    const backImage = document.querySelector(".image-container");
+    backImage.style.backgroundImage = `url(${images[4-run]})`;
+
+    if( run == 1 ){
+        document.documentElement.style.setProperty("--dark-blue" , "white");
+        document.documentElement.style.setProperty("--dark-desaturated-blue", "hsl(236, 33%, 92%)" );
+        document.documentElement.style.setProperty("--text-color", "hsl(235, 21%, 11%)" );
+        document.documentElement.style.setProperty("--brighter", "black" );
+        document.documentElement.style.setProperty("--circle-color", "hsl(233, 11%, 84%)" );
     }
-    document.getElementById('input').value = '';
-}
+    else {
+        document.documentElement.style.setProperty("--dark-blue" , "hsl( 235, 21% , 11% )");
+        document.documentElement.style.setProperty("--dark-desaturated-blue", "hsl(235, 24%, 19%)" );
+        document.documentElement.style.setProperty("--text-color", "rgba(255, 255, 255, 0.623)" );
+        document.documentElement.style.setProperty("--brighter", "white" );
+        document.documentElement.style.setProperty("--circle-color", "rgba(255, 255, 255, 0.11)" );
+    }
+
+})
